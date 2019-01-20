8186206dda1dae9c3994825b7a90fc67fb9233c5
ArtjomBobkevich <46691828+ArtjomBobkevich@users.noreply.github.com>
2019-01-20 15:19:23 +0200

    home

---
+++ Strochni_massiw/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//String mas[][] = {{"i","was","here","yesterday"},
+	//	{"and","where","were","you"}};
+//
+//	for(String s[]:mas)
+//	    System.out.print(Arrays.toString(s));
+//	System.out.println();
+		int sum=0;
+		int mas[] = {1,2,3,4,5};
+		for(int s:mas) {
+			System.out.print(s + " ");
+			//System.out.println();
+			sum += s;
+		}
+		System.out.println();
+		System.out.println(sum);
+    }
+}
