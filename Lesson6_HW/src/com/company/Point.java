e61d84edbbafc733f50681dc600cb8f1fb672910
ArtjomBobkevich <wassax@yandex.ru>
2019-02-05 00:23:38 +0300

    HW6 complite

--- Lesson6_HW/src/com/company/Point.java
+++ Lesson6_HW/src/com/company/Point.java
@@ -14,14 +14,26 @@ public class Point {
         return x;
     }
 
+<<<<<<< HEAD
+=======
 
+>>>>>>> 9d7c1d0619d81a3f58d523370435ccf465464e7b
     public int getY() {
         return y;
     }
 
+<<<<<<< HEAD
+    public double distanceX (Point x){
+        return this.x - x.getX();
+    }
+
+    public double distanceY (Point x){
+        return this.y - x.getY();
+=======
 
     public void distance(Point x) {
 
+>>>>>>> 9d7c1d0619d81a3f58d523370435ccf465464e7b
     }
 }
 
--- Lesson6_HW/src/com/company/Rectangle.java
+++ Lesson6_HW/src/com/company/Rectangle.java
@@ -1,6 +1,27 @@
 package com.company;
 
 public class Rectangle {
+<<<<<<< HEAD
+
+    private Point point1;
+    private Point point2;
+
+    public Rectangle (Point point1,Point point2){
+        this.point1=point1;
+        this.point2=point2;
+    }
+
+    public double yardageRectangle(Point point1, Point point2) {
+        return point1.distanceX(point2) * point1.distanceY(point2);
+    }
+
+    public double lengthDiagonals(Point point1, Point point2) {
+        return Math.sqrt((Math.pow(point1.distanceX(point2), 2) + Math.pow(point1.distanceY(point2), 2)));
+    }
+
+}
+
+=======
 
     private Point point1;
     private Point point2;
@@ -17,3 +38,4 @@ public class Rectangle {
 //        this.point2 = point2;
 
 
+>>>>>>> 9d7c1d0619d81a3f58d523370435ccf465464e7b
